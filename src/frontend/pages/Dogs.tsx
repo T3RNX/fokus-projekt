cca91759a34448185799d2dd9c03a0ecf02b84ea
Terence Balestrieri <terence.balestrieri@isolutions.ch>
2025-04-09 08:41:27 +0200

    Modify Dogs Display Layout for big screens

--- src/frontend/pages/Dogs.tsx
+++ src/frontend/pages/Dogs.tsx
@@ -77,7 +77,7 @@ const Dogs = () => {
           <p className="mt-2">Klicken Sie auf +, um einen Hund hinzuzufügen</p>
         </div>
       ) : (
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-4 mx-auto">
           {dogs.map((dog) => (
             <DogCard key={dog.dogID} {...dog} onDelete={handleDeleteDog} />
           ))}
